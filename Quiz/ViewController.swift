42c0d021d28497716f280696e5105a2bf6f2edc7
ShehriyarShariq <asiddiqui.bscs18seecs@seecs.edu.pk>
2021-08-29 17:02:44 -0600

    Initial Commit

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Shehriyar Shariq on 30/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    let questions : [String] = [
+        "What is 7 + 7?",
+        "What is the capital of Vermont",
+        "What is cognac made of?"
+    ]
+    
+    let answers : [String] = [
+        "14",
+        "Montelier",
+        "Grapes"
+    ]
+    
+    var currentQuestionIndex : Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        questionLabel.text = questions[currentQuestionIndex]
+    }
+
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        currentQuestionIndex += 1
+        
+        if (currentQuestionIndex == questions.count) {
+            currentQuestionIndex = 0
+        }
+        
+        let question : String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        
+        answerLabel.text = "???"
+    }
+    
+    @IBAction func showAnswer(_ sender: UIButton) {
+        let answer : String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+}
+
